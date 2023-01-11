94b55fa706abfa22b35c5edfc3b70e090b360ac5
FelixOtienoArogo <felixarogo@gmail.com>
2023-01-11 12:45:59 +0300

    ch

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,5 +1,5 @@
-# This Puppet manifest installs puppet-lint
+# installing a package using puppet
 package { 'puppet-lint':
-ensure   => '2.1.1',
-provider => 'gem',
+  ensure   => '2.1.1',
+  provider => 'gem',
 }
